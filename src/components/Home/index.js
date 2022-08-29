898c8946888b07b5d714bcbc40b1e7e24dc27e03
kristinavlasovets <61822704+kristinavlasovets@users.noreply.github.com>
2022-08-29 15:05:36 +0300

    created animatedletters component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,28 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { AnimatedLetters } from "../AnimatedLetters";
 import './index.scss'
-import PoolOne from '../../assets/images/pool-one.jpg'
 
 export const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = ['e', 'q', 'u', 'i', 'n', 'o', 'x'];
+
+  // useEffect(() => {
+  //   return setTimeout(() => {
+  //     setLetterClass('text-animate-hover')
+  //   }, 4000)
+  // }, [])
+
   return (
     <div className='container home-page'>
+    
       <div className='text-zone'>
-        <img src={PoolOne} alt="pool" />
+        
+         <AnimatedLetters  letterClass={letterClass} strArray={nameArray} idx={7}/>
+      </div>
+      <div className='button-zone'>
         <Link to="/contact" className="flat-button">Claim offer</Link>
       </div>
-
     </div>
   )
 }
